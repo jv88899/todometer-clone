ae353bdc42b50f7a50b74e85b3affc711149f831
jv88899 <jv88899@gmail.com>
2020-08-24 20:17:31 -0500

    made todo meter work

--- src/App.js
+++ src/App.js
@@ -29,7 +29,36 @@ function App() {
 			<Date />
 			<div className="w-full h-4">
 				<div className="flex bg-purple-700 h-full mt-8 mx-4 rounded-sm">
-					{}
+					{todos
+						.sort((a, b) => (a.status > b.status ? 1 : -1))
+						.map((todo) => {
+							if (todo.status === 1) {
+								return (
+									<div
+										key={todo.text}
+										className="h-4 bg-green-500 flex-grow"
+									>
+										{" "}
+									</div>
+								);
+							} else if (todo.status === 2) {
+								return (
+									<div
+										key={todo.text}
+										className="h-4 bg-yellow-500 flex-grow"
+									></div>
+								);
+							} else if (todo.status === 3) {
+								return (
+									<div
+										key={todo.text}
+										className="h-4 flex-grow"
+									></div>
+								);
+							} else {
+								return null;
+							}
+						})}
 				</div>
 			</div>
 		</div>
