0e19c078b4998bf3fa2c79796efefc55b461fa4d
jv88899 <jv88899@gmail.com>
2020-08-24 20:02:13 -0500

    added test data to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Date from "./components/Date";
 
 function App() {
+	const [todos, setTodos] = useState([
+		{
+			text: "This is my first todo",
+			status: 1,
+		},
+		{
+			text: "This is my second todo",
+			status: 1,
+		},
+		{
+			text: "This is my third todo",
+			status: 1,
+		},
+		{
+			text: "This is my fourth todo",
+			status: 2,
+		},
+		{
+			text: "This is my fifth todo",
+			status: 3,
+		},
+	]);
 	return (
 		<div className="min-h-screen max-h-full w-full bg-purple-900">
 			<Date />
 			<div className="w-full h-4">
-				<div className="flex bg-purple-700 h-full mt-8 mx-4 rounded-sm"></div>
+				<div className="flex bg-purple-700 h-full mt-8 mx-4 rounded-sm">
+					{}
+				</div>
 			</div>
 		</div>
 	);
