f200f90fef0391c5a46be15821d74e056d820131
jv88899 <jv88899@gmail.com>
2020-08-24 19:30:47 -0500

    added the date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function App() {
-	return <div>App</div>;
+	return (
+		<div className="min-h-screen max-h-full w-full bg-purple-900">
+			<div className="flex justify-between h-16 text-gray-100 mx-4 pt-4">
+				<div className="flex">
+					<span className="h-16 flex justify-center items-center text-5xl mr-2">
+						<p>2</p>
+					</span>
+					<div className="h-16 flex flex-col justify-center">
+						<span>Mar</span>
+						<span>2020</span>
+					</div>
+				</div>
+				<div className="h-16 flex justify-center items-center">
+					<p>Monday</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
