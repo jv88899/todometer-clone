e1d56ab95b42bd944134029d31d37468e39c9dc7
jv88899 <jv88899@gmail.com>
2020-08-24 19:41:35 -0500

    made dates in date component dynamic

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,19 +1,21 @@
 import React from "react";
+import moment from "moment";
 
 const Date = () => {
+	const todaysDate = moment();
 	return (
 		<div className="flex justify-between h-16 text-gray-100 mx-4 pt-4">
 			<div className="flex">
 				<span className="h-16 flex justify-center items-center text-5xl mr-2">
-					<p>2</p>
+					<p>{todaysDate.format("D")}</p>
 				</span>
 				<div className="h-16 flex flex-col justify-center">
-					<span>Mar</span>
-					<span>2020</span>
+					<span>{todaysDate.format("MMM")}</span>
+					<span>{todaysDate.format("yyyy")}</span>
 				</div>
 			</div>
 			<div className="h-16 flex justify-center items-center">
-				<p>Monday</p>
+				<p>{todaysDate.format("dddd")}</p>
 			</div>
 		</div>
 	);
