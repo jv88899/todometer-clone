a5de75e2681fbe51c8356a83018698bcb74003fb
jv88899 <jv88899@gmail.com>
2020-08-24 20:36:22 -0500

    turned the meter into a component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Date from "./components/Date";
+import Meter from "./components/Meter";
 
 function App() {
 	const [todos, setTodos] = useState([
@@ -27,40 +28,7 @@ function App() {
 	return (
 		<div className="min-h-screen max-h-full w-full bg-purple-900">
 			<Date />
-			<div className="w-full h-4">
-				<div className="flex bg-purple-700 h-full mt-8 mx-4 rounded-sm">
-					{todos
-						.sort((a, b) => (a.status > b.status ? 1 : -1))
-						.map((todo) => {
-							if (todo.status === 1) {
-								return (
-									<div
-										key={todo.text}
-										className="h-4 bg-green-500 flex-grow"
-									>
-										{" "}
-									</div>
-								);
-							} else if (todo.status === 2) {
-								return (
-									<div
-										key={todo.text}
-										className="h-4 bg-yellow-500 flex-grow"
-									></div>
-								);
-							} else if (todo.status === 3) {
-								return (
-									<div
-										key={todo.text}
-										className="h-4 flex-grow"
-									></div>
-								);
-							} else {
-								return null;
-							}
-						})}
-				</div>
-			</div>
+			<Meter todos={todos} />
 		</div>
 	);
 }
