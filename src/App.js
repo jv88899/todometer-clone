3ccd50ae9537be2ba73b911b5febb4e358da10d7
jv88899 <jv88899@gmail.com>
2020-08-24 21:20:53 -0500

    added form to add a todo

--- src/App.js
+++ src/App.js
@@ -30,6 +30,28 @@ function App() {
 		<div className="min-h-screen max-h-full w-full bg-purple-900">
 			<Date />
 			<Meter todos={todos} />
+			<form className="flex items-center justify-between mx-4 mt-4 h-16 bg-purple-600">
+				<input
+					type="text"
+					className="h-10 border-0 w-10/12 bg-purple-600"
+				/>
+				<button
+					type="submit"
+					className="w-2/12 flex justify-center text-gray-100"
+				>
+					<svg
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						className="plus w-10 h-10"
+					>
+						<path
+							fillRule="evenodd"
+							d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</button>
+			</form>
 			<div className="h-16 mx-4 mt-6">
 				{todos.map((todo) => (
 					<Todo todo={todo} />
