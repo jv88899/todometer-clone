37402c734caa874c37fea1db3de72eb5bd217293
jv88899 <jv88899@gmail.com>
2020-08-24 19:54:41 -0500

    added meter to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
 	return (
 		<div className="min-h-screen max-h-full w-full bg-purple-900">
 			<Date />
+			<div className="w-full h-4">
+				<div className="flex bg-purple-700 h-full mt-8 mx-4 rounded-sm"></div>
+			</div>
 		</div>
 	);
 }
