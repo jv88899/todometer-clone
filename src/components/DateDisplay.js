429f9f5eb00277af6a2631dfd54088a054016ec7
jv88899 <jv88899@gmail.com>
2020-09-09 08:15:37 -0500

    adjusted font color in date display component

--- src/components/DateDisplay.js
+++ src/components/DateDisplay.js
@@ -7,11 +7,13 @@ const Date = () => {
 		<div className="flex justify-between h-16 text-gray-100 mx-4 pt-4">
 			<div className="flex">
 				<span className="h-16 flex justify-center items-center text-5xl mr-2">
-					<p>{todaysDate.format("D")}</p>
+					<p className="text-gray-300">{todaysDate.format("D")}</p>
 				</span>
 				<div className="h-16 flex flex-col justify-center">
 					<span>{todaysDate.format("MMM")}</span>
-					<span>{todaysDate.format("yyyy")}</span>
+					<span className="text-gray-300">
+						{todaysDate.format("yyyy")}
+					</span>
 				</div>
 			</div>
 			<div className="h-16 flex justify-center items-center">
