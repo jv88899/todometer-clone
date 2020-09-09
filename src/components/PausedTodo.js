0f7422a601621e9efd9e1fdba65f4c645937b5c6
jv88899 <jv88899@gmail.com>
2020-09-08 20:22:48 -0500

    added play icon to Paused todos

--- src/components/PausedTodo.js
+++ src/components/PausedTodo.js
@@ -44,11 +44,11 @@ const PausedTodo = ({
 			<svg
 				viewBox="0 0 20 20"
 				fill="currentColor"
-				className="pause w-6 h-6 text-yellow-500"
+				className="play w-6 h-6 text-yellow-500"
 			>
 				<path
 					fillRule="evenodd"
-					d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z"
+					d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
 					clipRule="evenodd"
 				/>
 			</svg>
