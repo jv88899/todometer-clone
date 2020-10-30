1b95a7bba5086a36b88a5c146a0dc32b6b34d68d
jv88899 <jv88899@gmail.com>
2020-10-30 10:06:01 -0500

    added firebase information

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+var firebaseConfig = {
+	apiKey: "AIzaSyArskq4mOeQHmweabb-jfmzPBprOEYCkhk",
+	authDomain: "tdc-app-cafb5.firebaseapp.com",
+	databaseURL: "https://tdc-app-cafb5.firebaseio.com",
+	projectId: "tdc-app-cafb5",
+	storageBucket: "tdc-app-cafb5.appspot.com",
+	messagingSenderId: "745195701206",
+	appId: "1:745195701206:web:db9416bc30b79e506c5a77",
+	measurementId: "G-YWQ9P6NVSX",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
